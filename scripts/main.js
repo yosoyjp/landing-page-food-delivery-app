831eaa83aece03e24708fcb7b9289e3333239ced
Johnny <johnnye96pv@gmail.com>
2021-01-28 00:02:25 -0500

    style header for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 window.onload = function () {
     let togleMenu = false;
+    const menuList = document.getElementById('list-options-header');
     const menuTogleButton = document.getElementsByClassName('toggle-menu')[0];
     menuTogleButton.addEventListener('click', function () {
             togleMenu = !togleMenu;
             hidden(menuTogleButton.children [togleMenu ? 0 : 1]);
             show(menuTogleButton.children [!togleMenu ? 0 : 1]);
+            if(togleMenu) menuList.classList.add('show');
+            else menuList.classList.remove('show');
         });
 };
 
